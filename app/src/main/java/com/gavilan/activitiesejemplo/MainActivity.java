c4914ae98bf40ac84c7be5e601273f54875726b2
Jotape <j.p.daniel.gavilan@gmail.com>
2021-09-07 18:15:59 -0300

    APP de ejemplo para multiples activities

---
+++ app/src/main/java/com/gavilan/activitiesejemplo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.gavilan.activitiesejemplo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // Métodos que funcionan al hacer click en los btns
+
+    public void abrirFormulario(View view){
+        Intent intent = new Intent(MainActivity.this,ActivityFormulario.class);
+        startActivity(intent);
+    }
+
+    public void abrirListado(View view){
+        Intent intent = new Intent(MainActivity.this,ActivityListado.class);
+        startActivity(intent);
+    }
+
+}
